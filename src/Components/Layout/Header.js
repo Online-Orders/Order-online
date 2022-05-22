a5a8ac33c66c11e5f2134c1a0b319fee3c0c70ac
Niraj <amatyaraj@gmail.com>
2022-05-22 23:07:57 +1000

    Meal image added to the Navbar and initial structure forNavbar created with css

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,10 +1,17 @@
 import classes from './Header.module.css';
+import mealImage from '../../assets/meals.jpg';
 
 const Header = (props) => {
   return (
-    <nav>
-      <h2>OrderOnline</h2>
-    </nav>
+    <>
+      <nav className={classes.header}>
+        <h2>OrderOnline</h2>
+        <button>Your Cart 2</button>
+      </nav>
+      <div className={classes['main-image']}>
+        <img src={mealImage} alt="delicious food" />
+      </div>
+    </>
   );
 };
 

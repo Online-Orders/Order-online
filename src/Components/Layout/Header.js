606f516aae8849baaa41d7c89f0ec1210e943fe7
Niraj <amatyaraj@gmail.com>
2022-05-22 23:22:04 +1000

    design layout for Navbar and header cart button

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,12 +1,13 @@
 import classes from './Header.module.css';
 import mealImage from '../../assets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
   return (
     <>
       <nav className={classes.header}>
         <h2>OrderOnline</h2>
-        <button>Your Cart 2</button>
+        <HeaderCartButton />
       </nav>
       <div className={classes['main-image']}>
         <img src={mealImage} alt="delicious food" />

2979cb4c2b885f9e92710d6058f38e670647873b
Niraj <amatyaraj@gmail.com>
2022-05-23 11:24:01 +1000

    price in the view will only show two decimal points and with dollar sign in the front

--- src/Components/Meals/MealsList/MealList.js
+++ src/Components/Meals/MealsList/MealList.js
@@ -1,12 +1,14 @@
 import classes from './MealList.module.css';
 
 const MealList = (props) => {
+  // price with decimal upto 2 decimal points and with $ in the front in each price
+  const mealPrice = `$${props.price.toFixed(2)}`;
   return (
     <li className={classes.meal}>
       <div>
         <h3>{props.mealName}</h3>
         <div className={classes.description}>{props.description}</div>
-        <div className={classes.price}>{props.price}</div>
+        <div className={classes.price}>{mealPrice}</div>
       </div>
     </li>
   );

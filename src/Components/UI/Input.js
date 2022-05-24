515be04fa39d97ce8a99851141a3723deb8effde
Niraj <amatyaraj@gmail.com>
2022-05-24 23:13:33 +1000

    Reusable Input button

---
+++ src/Components/UI/Input.js
@@ -0,0 +1,10 @@
+const Input = (props) => {
+  return (
+    <>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input {...props.input} />
+    </>
+  );
+};
+
+export default Input;

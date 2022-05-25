779498a47cf991052632316779b40a1f8b30537d
Niraj <amatyaraj@gmail.com>
2022-05-25 23:26:48 +1000

    changed hard coded price for total

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -28,7 +28,7 @@ const Cart = () => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>40.50</span>
+        <span>50.50</span>
       </div>
       <div className={classes.actions}>
         <button>Close</button>

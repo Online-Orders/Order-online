37b693d23ddb575e854964e3121f78d102020518
Niraj <amatyaraj@gmail.com>
2022-06-02 14:11:56 +1000

    Backdrop and Modal Layout components with styling
     These components will be used to create a Modal

--- src/Components/UI/Modal.js
+++ src/Components/UI/Modal.js
@@ -1,3 +1,12 @@
+import classes from './Modal.module.css';
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModalLayout = (props) => {
+  return <div className={classes.modal}>{props.children}</div>;
+};
+
 const Modal = (props) => {
   return;
 };

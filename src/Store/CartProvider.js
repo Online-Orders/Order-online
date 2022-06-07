55cdca4865a9fdb98c56e3e155e00ce344058a86
Niraj <amatyaraj@gmail.com>
2022-06-07 13:51:10 +1000

    apply useReducer hooks
    useReducer hook is used to manage the state of the cart. useReducer helps to manage the state management logic in separate component. That way you can keep state management seperate from rendering logic.
    
    CartProvider value is updated by current cart state from useReducer. So that all the children component having access to the provider will always get the recent state.

--- src/Store/CartProvider.js
+++ src/Store/CartProvider.js
@@ -1,12 +1,39 @@
+import { useReducer } from 'react';
 import CartContext from './CartContext';
+
+// initital state of the cart that is passed to useReducer current state
+const initialState = {
+  items: [],
+  totalAmount: 0,
+};
+
+// reducer function to manage the state of the cart.
+// accepts state and action, returns the current state
+const cartReducer = (state, action) => {
+  return {
+    initialState,
+  };
+};
 
 // Cart Provider Component that comes from the context enables consuming components to subscribe to the state/context changes
 const CartProvider = (props) => {
+  // useReducer to manage the state
+  // takes two argument reducer function and initial state
+  // returns current state and dispatch function
+  const [cartState, dispatchCart] = useReducer(cartReducer, initialState);
+
+  // adds new item to the cart
+  const addItemToCartHandler = (item) => {};
+
+  //removes item from the cart
+  const removeCartItemHandler = (id) => {};
+
+  // updating value of the provider with the current state
   const cartContext = {
-    items: [],
-    total: 0,
-    addItem: (item) => {},
-    removeItem: (id) => {},
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeCartItemHandler,
   };
   return (
     //   value props passed to all the child components

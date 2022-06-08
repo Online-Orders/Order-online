b8df975d9311f776960f1e5cdbd9284e5e1bc6fa
Niraj <amatyaraj@gmail.com>
2022-06-08 11:38:15 +1000

    use event handler
    onChange event handler is used and passed the handler function through props, from MealForm Component.
    EveryChange is captured and state is updated.

--- src/Components/UI/Input.js
+++ src/Components/UI/Input.js
@@ -4,7 +4,8 @@ const Input = (props) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input {...props.input} />
+      {/*setting onChange event listener and value and passed the props from the MealForm */}
+      <input {...props.input} onChange={props.onChange} value={props.value} />
     </div>
   );
 };

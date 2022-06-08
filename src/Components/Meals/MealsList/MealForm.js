cd69a13f1e7528a3f59484fc151a7431e07e5afc
Niraj <amatyaraj@gmail.com>
2022-06-08 11:29:18 +1000

    feat: add quantity for item
    Quantity for the item added in the cart can be selected
    The form is controlled form and uses useState to manage the useState
    Also, on submitting the form when user adds the quantity, it is passed to the MealList Component, where it is handled.

--- src/Components/Meals/MealsList/MealForm.js
+++ src/Components/Meals/MealsList/MealForm.js
@@ -1,9 +1,31 @@
+import { useState } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealForm.module.css';
 
 const MealForm = (props) => {
+  // sets the inital value of the state
+  // also this is the inital value of input form
+  const initialValue = 1;
+  const [qtyAdded, setQtyAdded] = useState(initialValue);
+
+  // On add button clicked in the form
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // changes string to integer
+    const qty = +qtyAdded;
+    // pass the value to the function passed as a prop from MealList, where it is handled
+    props.handleItemQty(qty);
+    // state is setback to the initialValue
+    setQtyAdded(initialValue);
+  };
+
+  // everytime the input value changes, it updates the state
+  const handleChange = (event) => {
+    setQtyAdded(event.target.value);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={handleSubmit}>
       <Input
         label="Amount"
         input={{
@@ -12,10 +34,12 @@ const MealForm = (props) => {
           step: '1',
           min: '1',
           max: '5',
-          defaultValue: '1',
         }}
+        // passing
+        onChange={handleChange}
+        value={qtyAdded}
       />
-      <button>+Add</button>
+      <button type="submit">+Add</button>
     </form>
   );
 };

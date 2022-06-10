c2f28ad8073032faefac9ef70d355fade199fd79
Niraj <amatyaraj@gmail.com>
2022-06-11 00:24:19 +1000

    feat: Add item using useRef hook
    useRef used insted of useState to get the current state from the dom
    ref passed as props to Input Components
    value is extracted using current.value, comes with useRef.

--- src/Components/Meals/MealsList/MealForm.js
+++ src/Components/Meals/MealsList/MealForm.js
@@ -1,32 +1,50 @@
-import { useState } from 'react';
+// import { useState } from 'react';
+import { useRef } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealForm.module.css';
 
 const MealForm = (props) => {
+  // ************ state management using useState ***************
+
   // sets the inital value of the state
   // also this is the inital value of input form
-  const initialValue = 1;
-  const [qtyAdded, setQtyAdded] = useState(initialValue);
+
+  // const initialValue = 1;
+  // const [qtyAdded, setQtyAdded] = useState(initialValue);
 
   // On add button clicked in the form
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // changes string to integer
-    const qty = +qtyAdded;
-    // pass the value to the function passed as a prop from MealList, where it is handled
-    props.handleItemQty(qty);
-    // state is setback to the initialValue
-    setQtyAdded(initialValue);
-  };
+
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   // changes string to integer
+  //   const qty = +qtyAdded;
+  //   // pass the value to the function passed as a prop from MealList, where it is handled
+  //   props.handleItemQty(qty);
+  //   // state is setback to the initialValue
+  //   setQtyAdded(initialValue);
+  // };
 
   // everytime the input value changes, it updates the state
-  const handleChange = (event) => {
-    setQtyAdded(event.target.value);
+  // const handleChange = (event) => {
+  //   setQtyAdded(event.target.value);
+  // };
+
+  // ********** getting current state using useRef. Uncontrolled form uses useRef ************
+
+  const qtyRef = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const qtyAdded = qtyRef.current.value;
+    const qtyAddedNumber = +qtyAdded;
+    console.log(qtyAddedNumber);
+    props.handleItemQty(qtyAddedNumber);
   };
 
   return (
     <form className={classes.form} onSubmit={handleSubmit}>
       <Input
+        ref={qtyRef}
         label="Amount"
         input={{
           id: 'amount_' + props.id,
@@ -34,10 +52,13 @@ const MealForm = (props) => {
           step: '1',
           min: '1',
           max: '5',
+          // defaultValue is used with useRef as the form is uncontrolled form
+          defaultValue: '1',
         }}
-        // passing
-        onChange={handleChange}
-        value={qtyAdded}
+        // passing the function handleChange and value to the Input UI, when the form is controlled form
+        // onChange={handleChange}
+
+        // value={qtyAdded}
       />
       <button type="submit">+Add</button>
     </form>

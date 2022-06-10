259e5992aa2a452e1f5c2dc5d61182aa750e1ede
Niraj <amatyaraj@gmail.com>
2022-06-11 00:29:37 +1000

    use forwardRef
    ref cannot be passed to child Components
    forwardRef from React is used to pass the ref to the Input component

--- src/Components/UI/Input.js
+++ src/Components/UI/Input.js
@@ -1,13 +1,19 @@
+import React from 'react';
 import classes from './Input.module.css';
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
       {/*setting onChange event listener and value and passed the props from the MealForm */}
-      <input {...props.input} onChange={props.onChange} value={props.value} />
+      <input
+        {...props.input}
+        // onChange={props.onChange}
+        // value={props.value}
+        ref={ref}
+      />
     </div>
   );
-};
+});
 
 export default Input;

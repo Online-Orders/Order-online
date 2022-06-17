2c4e58ed50c7e1a7aeb4734f38e2d11ef8f43322
Niraj <amatyaraj@gmail.com>
2022-06-18 00:06:16 +1000

    feat: updater total number of items in HeaderCartButton
    total number of items were not updated in the headerCartButton when item was added to the cart. Reduce funciton is used to, as it provides sum of the all the items quantity added to the cart and provides single value. Everytime each item is added to the cart, the total quantity gets updated in the headerCartButton.

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,7 +1,15 @@
+import { useContext } from 'react';
+import CartContext from '../../Store/CartContext';
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
 
 const HeaderCartButton = (props) => {
+  const ctx = useContext(CartContext);
+  const { items } = ctx;
+
+  const totalQtyToCart = items.reduce((previousQty, currentItem) => {
+    return previousQty + currentItem.qty;
+  }, 0);
   return (
     <>
       <button className={classes.button} onClick={props.showModal}>
@@ -9,7 +17,7 @@ const HeaderCartButton = (props) => {
           <CartIcon />
         </span>
         <span>Your Cart</span>
-        <span className={classes.badge}>2</span>
+        <span className={classes.badge}>{totalQtyToCart}</span>
       </button>
     </>
   );

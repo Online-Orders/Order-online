b53ff420d5fafb491d7cbc357a614286294da19e
Niraj <amatyaraj@gmail.com>
2022-07-16 16:24:09 +1000

    refactor: add confirm and cancel button
    - add confirm and cancel button to the chekout Checkout Checkout Form

--- src/Components/Cart/CheckoutForm.js
+++ src/Components/Cart/CheckoutForm.js
@@ -4,31 +4,35 @@ const CheckoutForm = () => {
   return (
     <>
       <h3>Plese enter your details.</h3>
-      <form className={classes['form-control']} action="">
-        <div className={classes.control}>
-          <label htmlFor="name">Your Name</label>
-          <input type="text" id="name" />
+      <form>
+        <div className={classes['form-control']}>
+          <div className={classes.control}>
+            <label htmlFor="name">Your Name</label>
+            <input type="text" id="name" />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="street">Street</label>
+            <input type="text" id="street" />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="suburb">Suburb</label>
+            <input type="text" id="suburb" />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="state">State</label>
+            <input type="text" id="state" />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="post-code">Post Code</label>
+            <input type="text" id="post-code" />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="country">Country</label>
+            <input type="text" id="country" />
+          </div>
         </div>
-        <div className={classes.control}>
-          <label htmlFor="street">Street</label>
-          <input type="text" id="street" />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="suburb">Suburb</label>
-          <input type="text" id="suburb" />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="state">State</label>
-          <input type="text" id="state" />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="post-code">Post Code</label>
-          <input type="text" id="post-code" />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="country">Country</label>
-          <input type="text" id="country" />
-        </div>
+        <button>Confirm</button>
+        <button>Cancel</button>
       </form>
     </>
   );

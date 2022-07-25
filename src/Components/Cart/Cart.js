6626eb62a3f73305d088e64110b3349c955041dc
Niraj <amatyaraj@gmail.com>
2022-07-26 07:17:13 +1000

    feat: render checkout form
    - renders checkout form in the cart when user clicks on the order button.
    - state is managed to check if order button is clicked by user. It's given boolean value of "false" initially.
    - once order button is clicked it is set to "true".
    - this is to make sure that checkout form is only shown when user decides to order and not otherwise.

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,6 +2,7 @@ import { useContext, useState } from 'react';
 import CartContext from '../../Store/CartContext';
 import CartItem from './CartItem';
 import CheckoutForm from './CheckoutForm';
+import useHttp from '../../Hooks/use-http';
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 
@@ -61,23 +62,52 @@ const Cart = (props) => {
   // checking if any item is added to the cart. This will return true or false
   const cartHasItem = items.length > 0;
 
+  // check if order button is clicked helper
+
+  /* using ternary opertor to check if order button is clicked, and if it true, show the checkout form and hide the order and cancel button. */
+
+  const checkOrderButtonIsClicked = orderButtonIsClicked ? (
+    // div is applied here so that it only applies to the checkout form and not to the empty string.
+    // otherwise div will be applied to the empty string and will be visible even when the order button is not clicked, since there is some padding applied to the div.
+    <div className={classes.containerTwo}>
+      <CheckoutForm hideModal={props.hideModal} />
+    </div>
+  ) : (
+    ''
+  );
+
+  // helper function to show "order" and "button" when order button is not clicked and the state orderButtonIsClicked is false
+  const orderAndCloseButtonAreVisible = !orderButtonIsClicked && (
+    <div className={classes.actions}>
+      {/* ternary operator to check if cartHasItem is true or false and only show Order button if it is true */}
+      {cartHasItem ? <button onClick={showForm}>Order</button> : ''}
+      {/* closes the modal */}
+      <button onClick={props.hideModal}>Close</button>
+    </div>
+  );
+
+  // helper class to style, when checkout form is shown after order button is clicked
+  const helperClasses = orderButtonIsClicked ? `${classes.container}` : '';
+
   return (
-    <Modal onClose={props.hideModal}>
-      {cartItems}
-      <div className={classes.total}>
-        <span>Total Amount</span>
-        <span>{total}</span>
-      </div>
-      {/* using ternary opertor to check if order button is clicked, and if it true show the checkout form and hide the order and cancel button. */}
-      {orderButtonIsClicked ? (
-        <CheckoutForm hideModal={props.hideModal} />
-      ) : (
-        <div className={classes.actions}>
-          <button onClick={props.hideModal}>Close</button>
-          {/* ternary operator to check if cartHasItem is true or false and only show Order button if it is true */}
-          {cartHasItem ? <button onClick={showForm}>Order</button> : ''}
+    <Modal
+      onClose={props.hideModal}
+      // orderButtonIsClicked state is passed to Modal so that styling is done depending on if checkout form is shown or hidden
+      // handleOrderButtonClicked={orderButtonIsClicked}/////////////////////
+    >
+      <div className={helperClasses}>
+        <div className={classes.containerOne}>
+          {cartItems}
+          <div className={classes.total}>
+            <span>Total Amount</span>
+            <span>{total}</span>
+          </div>
+          {/* Order and close button is only visible until Order button is clicked. Once Order button is clicked they disappear and checkout form is visible */}
+          {orderAndCloseButtonAreVisible}
         </div>
-      )}
+        {/* div on this is applied to the helper function 'checkOrderButtonIsClicked' above */}
+        {checkOrderButtonIsClicked}
+      </div>
     </Modal>
   );
 };

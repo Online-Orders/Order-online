059a0668201203e28b904710b888dcd6ee8213e8
Niraj <amatyaraj@gmail.com>
2022-07-27 06:59:03 +1000

    add onBlur and onFocus to the form field to show error message
    - onBlur is used to show error message when user looses focus from the input field and clicks somewhere else
    - onFocus is only used for confirm password, so that unmatched error can be checked immediately and not wait until submit button is clicked.

--- src/Components/UI/CheckoutFormInput.js
+++ src/Components/UI/CheckoutFormInput.js
@@ -1,11 +1,33 @@
+import { useState } from 'react';
 import classes from './CheckoutFormInput.module.css';
 
 const CheckoutFormInput = (props) => {
+  const [inputIsTouched, setInputIstouched] = useState(false);
   const { onChange } = props;
+
+  // triggers when user looses focus from the input field and clickes somewhere else.
+  const handleBlur = (event) => {
+    setInputIstouched(true);
+  };
+
+  // handleFocus is only designed to work with confirmPassword field.
+  // if this field is not handled, confirmPassord error message won't pop up until "confirm button" is clicked. So user can type any thing in the input filed.
+  // confirmPassword should check the validity as soon as it matches with the original password.
+  const handleFocusOnConfirmPassword = (event) => {
+    props.name === 'confirmPassword' && setInputIstouched(true);
+  };
+
   return (
     <div key={props.id} className={classes.formInput}>
       <label>{props.label}</label>
-      <input onChange={onChange} {...props} />
+      <input
+        onChange={onChange}
+        {...props}
+        onBlur={handleBlur}
+        inputIsTouched={inputIsTouched.toString()}
+        // onFocus here will only trigger for confirm password
+        onFocus={handleFocusOnConfirmPassword}
+      />
       <span className={classes.errors}>{props.errorMessage}</span>
     </div>
   );
